10a8e8f758888f74ae070058b278b0bf8d73f091
Osmar Dominguez <osmardcx@gmail.com>
2018-04-01 20:01:48 -0500

    first commit

---
+++ app/assets/javascripts/fbsdk.js
@@ -0,0 +1,13 @@
+// $(document).ready(function() {
+//   $.ajaxSetup({ cache: true });
+//   $.getScript('https://connect.facebook.net/en_US/sdk.js', function(){
+//     FB.init({
+//       appId: '{your-app-id}',
+//       cookie     : true,
+//       xfbml      : true,
+//       version: 'v2.7' // or v2.1, v2.2, v2.3, ...
+//     });     
+//     $('#loginbutton,#feedbutton').removeAttr('disabled');
+//     FB.getLoginStatus(updateStatusCallback);
+//   });
+// });
